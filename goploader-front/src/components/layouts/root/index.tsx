20065e2ca934664f036b8d0dd3fe13ad78732630
Danilove <dansousa09@gmail.com>
2024-01-02 22:32:36 -0300

    feat: create the Home Page

--- goploader-front/src/components/layouts/root/index.tsx
+++ goploader-front/src/components/layouts/root/index.tsx
@@ -1,31 +1,18 @@
-import { Link, Outlet } from 'react-router-dom'
-import { ClerkProvider, SignedIn, SignedOut, UserButton } from '@clerk/clerk-react'
+import { Outlet } from "react-router-dom";
+import { ClerkProvider } from "@clerk/clerk-react";
 
-const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
 if (!PUBLISHABLE_KEY) {
-  throw new Error("Missing Publishable Key")
+	throw new Error("Missing Publishable Key");
 }
 
 export default function RootLayout() {
-  return (
-    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
-      <header className="header">
-        <div>
-          <div>
-            <p>Clerk + React + React Router App</p>
-          </div>
-          <SignedIn>
-            <UserButton afterSignOutUrl='/sign-in' />
-          </SignedIn>
-          <SignedOut>
-            <Link to="/sign-in">Sign In</Link>
-          </SignedOut>
-        </div>
-      </header>
-      <main>
-        <Outlet />
-      </main>
-    </ClerkProvider>
-  )
+	return (
+		<ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+			<main>
+				<Outlet />
+			</main>
+		</ClerkProvider>
+	);
 }
--- goploader-front/src/modules/Home/pages/index.tsx
+++ goploader-front/src/modules/Home/pages/index.tsx
@@ -1,24 +1,31 @@
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import { Button } from "../../../components/ui/button";
+import { Separator } from "../../../components/ui/separator";
 
 export default function HomePage() {
+	const navigate = useNavigate();
 	return (
-		<div>
-			<h1>This is the home page</h1>
-			<div>
-				<ul>
-					<li>
-						<Link to="/sign-up">Sign Up</Link>
-					</li>
-					<li>
-						<Link to="/sign-in">Sign In</Link>
-					</li>
-					<li>
-						<Link to="/contact">Contact</Link>
-					</li>
-					<li>
-						<Link to="/dashboard">Dashboard</Link>
-					</li>
-				</ul>
+		<div className="w-screen h-screen flex flex-col items-center justify-center bg-gradient-to-br from-indigo-500 to-indigo-700">
+			<h1 className="font-bold text-8xl text-white">
+				<span className="underline decoration-sky-500 mr-2 border rounded-xl p-4">
+					Go
+				</span>
+				ploader
+			</h1>
+			<Separator className="w-1/2 my-24" />
+			<div className="w-xl flex flex-col space-y-2">
+				<Button
+					className="border border-zinc-300 transition-all hover:shadow-xl hover:from-indigo-700 hover:to-indigo-500 active:from-indigo-500 active:to-indigo-700"
+					onClick={() => navigate("/dashboard")}
+				>
+					Start uploading
+				</Button>
+				<Button
+					className="border border-zinc-300 transition-all hover:shadow-xl hover:from-indigo-700 hover:to-indigo-500 active:from-indigo-500 active:to-indigo-700"
+					onClick={() => navigate("/contact")}
+				>
+					Contact
+				</Button>
 			</div>
 		</div>
 	);
