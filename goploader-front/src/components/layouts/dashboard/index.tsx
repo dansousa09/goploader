ab4aac17840f8618d704dc1299555cad60a384f8
Danilove <dansousa09@gmail.com>
2024-01-07 22:16:32 -0300

    feat: create the header of dashboard layout

--- goploader-front/src/components/layouts/dashboard/index.tsx
+++ goploader-front/src/components/layouts/dashboard/index.tsx
@@ -1,6 +1,9 @@
 import * as React from "react";
-import { useAuth } from "@clerk/clerk-react";
+import { UserButton, useAuth } from "@clerk/clerk-react";
 import { Outlet, useNavigate } from "react-router-dom";
+import { HelpCircle, Phone } from "lucide-react";
+import { Button } from "../../ui/button";
+import { Input } from "../../ui/input";
 
 export default function DashboardLayout() {
 	const { userId, isLoaded } = useAuth();
@@ -14,5 +17,31 @@ export default function DashboardLayout() {
 
 	if (!isLoaded) return "Loading...";
 
-	return <Outlet />;
+	return (
+		<div className="w-screen h-screen">
+			<header className="w-full h-20 p-6 flex items-center justify-between border-b border-zinc-200">
+				<h1 className="font-bold text-xl text-zinc-800">
+					<span className="underline decoration-indigo-500 mr-2 border-2 border-zinc-800 rounded-xl p-2">
+						Go
+					</span>
+					ploader
+				</h1>
+				<div className="w-1/3">
+					<Input placeholder="Pesquisar..." />
+				</div>
+				<div className="flex items-center space-x-4">
+					<Button variant="ghost" size="icon" className="w-8 h-8 p-1.5">
+						<Phone className="text-sm" />
+					</Button>
+					<Button variant="ghost" size="icon" className="w-8 h-8 p-1.5">
+						<HelpCircle className="text-sm" />
+					</Button>
+					<UserButton />
+				</div>
+			</header>
+			<main className="w-screen px-6 py-4">
+				<Outlet />
+			</main>
+		</div>
+	);
 }
