ba62b6fc445da079d2906c702e21a9c3b11ebf4e
Danilove <dansousa09@gmail.com>
2024-07-29 18:44:46 -0300

    feat(frontend): Update App component layout and styles

--- goploader-frontend/src/App.tsx
+++ goploader-frontend/src/App.tsx
@@ -1,34 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	return (
+		<div className="w-screen h-screen flex">
+			<aside className="hidden md:flex w-[15%] h-full bg-green-200">asd</aside>
+			<main className="flex-1 flex flex-col bg-red-200">
+				<header className="w-full h-16 bg-yellow-200">asd</header>
+				<div>asd</div>
+			</main>
+		</div>
+	);
 }
 
-export default App
+export default App;
